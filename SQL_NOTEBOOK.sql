1d4f79c8c8a9af1c9cbdf89a433735bb3ef634a4
jonathanjackson-1 <Jonathanjackson.work1@gmail.com>
2024-10-28 20:22:45 -0400

    Update SQL_NOTEBOOK.sql

--- SQL_NOTEBOOK.sql
+++ SQL_NOTEBOOK.sql
@@ -5,11 +5,14 @@ SQL USE CASE
 
 GOALS
 - Create database
+- Clean important tables for analysis
 - Pull summary statistics on churned customers and current customers
 - Create dashboard showing key churn rate metrics filtered by demographic
   '''
 
 -- Cleaning Processes
+-- Note: Most relevant analysis will be done by comparing demographic information of terminated customers to that of current customers. Most of these processes will specificially focus on these two tables
+  
 -- 1. Change Data type of column "HAS_CHILDREN" to boolean
 
 -- Decimal dropped from "HAS_CHILDREN" column in "demographic" table
