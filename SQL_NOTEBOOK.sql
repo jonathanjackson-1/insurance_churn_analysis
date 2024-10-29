132eadbe559ac153205756ab27cff7e5bdab86ed
jonathanjackson-1 <Jonathanjackson.work1@gmail.com>
2024-10-28 20:09:50 -0400

    Update SQL_NOTEBOOK.sql

--- SQL_NOTEBOOK.sql
+++ SQL_NOTEBOOK.sql
@@ -1,5 +1,19 @@
 '''
 SQL USE CASE
-- Create 
+- Create database for related tables
+- Clean/Manipulate all data
 
-'
+GOALS
+- Create database
+- Pull summary statistics on churned customers and current customers
+- Create dashboard showing key churn rate metrics filtered by demographic
+  '''
+
+-- Cleaning Processes
+-- 1. Change Data type of column "HAS_CHILDREN" to boolean
+
+-- Decimal dropped from "HAS_CHILDREN" column in "demographic" table
+UPDATE demographic
+SET "HAS_CHILDREN" = REPLACE("HAS_CHILDREN", '.0', '')
+
+
